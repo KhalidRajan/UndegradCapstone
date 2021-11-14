3f0cc576e9f0bf5c25fbfc4c35a3c2c82b77cbb8
Khalid Rajan <khalidrajan14@gmail.com>
2021-11-14 15:10:56 -0500

    Implement React Router

--- trade_marquis/src/App.js
+++ trade_marquis/src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
 import Navigation from "./navigation";
 import Landing from "./landing";
+import About from "./about";
+import Team from "./team";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
 
 
 function App(){
     return(
-        <div>
+        <BrowserRouter>
             <Navigation/>
-            <Landing/>
-        </div>
+            <Routes>
+                <Route path="/" element={<Landing/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/team" element={<Team/>}/>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
