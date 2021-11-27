64f79a255964fb671701ba48ac407a9d2f83b40b
KrishnaIyer99 <krishnaiyer28@gmail.com>
2021-11-27 12:09:41 -0500

    Basic functionality for trademark analyzer page

--- client/src/pages/tm_analyzer.js
+++ client/src/pages/tm_analyzer.js
@@ -1,9 +1,24 @@
 import React from "react";
+import Uploady from "@rpldy/uploady";
+import UploadButton from "@rpldy/upload-button"
 
 function TMAnalyze(){
     return(
         <div>
             <h1>Trademark Analyzer</h1>
+            <br></br>
+            <p>Upload trademark as image:</p>
+            <br></br>
+            <Uploady destination={{url: process.env.UPLOAD_URL}}>
+                <UploadButton>Upload image</UploadButton>
+            </Uploady>
+            <br></br>
+            <br></br>
+            <p>Upload word mark:</p>
+            <form>
+                <input type="text" id="word-mark" name="word-mark"></input>
+                <input type="submit" value="Submit"/>
+            </form>
         </div>
     )
 }
