3c552e5452b82dfa27f7d5dbdf34045b80ef5089
KrishnaIyer99 <krishnaiyer28@gmail.com>
2021-11-30 23:06:16 -0500

    Standard CSS for input pages

--- client/src/pages/nice_input.js
+++ client/src/pages/nice_input.js
@@ -1,20 +1,23 @@
 import React from "react";
-import "../styles-css/nice_input.css";
+import "../styles-css/input.css";
 
 function Form(){
     return(
-        <div className="form">
-            <h1><b>Input information:</b></h1>
-            <form>
-                <label>
+        <div className="background">
+            <div className="form">
+                <h1 className="title">Input information for NICE classifier</h1>
+                <form>
+                    <label><h1 className="title">Input information for NICE Classifier</h1></label>
+                    <label>
+                        <br></br>
+                        Describe the product(s) and/or service(s) that will be provided under this trademark:
+                        <br></br>
+                        <textarea cols="50" rows="10" id="descriptor" type="text" name="descriptor"> Input description...</textarea>
+                    </label>
                     <br></br>
-                    Describe the product(s) and/or service(s) that will be provided under this trademark:
-                    <br></br>
-                    <textarea cols="50" rows="10" id="descriptor" type="text" name="descriptor"> Input description...</textarea>
-                </label>
-                <br></br>
-                <input type="submit" value="Submit"/>
-            </form>
+                    <input type="submit" value="Submit"/>
+                </form>
+            </div>
         </div>
     )
 }
--- client/src/pages/tm_analyzer.js
+++ client/src/pages/tm_analyzer.js
@@ -1,24 +1,30 @@
 import React from "react";
 import Uploady from "@rpldy/uploady";
 import UploadButton from "@rpldy/upload-button"
+import "../styles-css/input.css";
 
 function TMAnalyze(){
     return(
-        <div>
-            <h1>Trademark Analyzer</h1>
-            <br></br>
-            <p>Upload trademark as image:</p>
-            <br></br>
-            <Uploady destination={{url: process.env.UPLOAD_URL}}>
-                <UploadButton>Upload image</UploadButton>
-            </Uploady>
-            <br></br>
-            <br></br>
-            <p>Upload word mark:</p>
-            <form>
-                <input type="text" id="word-mark" name="word-mark"></input>
-                <input type="submit" value="Submit"/>
-            </form>
+        <div className="background">
+            <div className="form">
+                <h1 className="title">Trademark Analyzer Input</h1>
+                <h1 className="title">Trademark Analyzer Input</h1>
+                <br></br>
+                <p>Upload trademark as image:</p>
+                <br></br>
+                <Uploady destination={{url: process.env.UPLOAD_URL}}>
+                    <UploadButton>Upload image</UploadButton>
+                </Uploady>
+                <br></br>
+                <br></br>
+                <p>Upload word mark:</p>
+                <form>
+                    <input type="text" id="word-mark" name="word-mark"></input>
+                    <br></br>
+                    <br></br>
+                    <input type="submit" value="Submit"/>
+                </form>
+            </div>
         </div>
     )
 }
