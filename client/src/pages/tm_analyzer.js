e6537c2e898689de16c3de6e62f2d9190ff3aebb
Khalid Rajan <khalidrajan14@gmail.com>
2022-03-10 18:45:43 -0500

    Add ability for user to upload multiple logos

--- client/src/pages/tm_analyzer.js
+++ client/src/pages/tm_analyzer.js
@@ -18,22 +18,39 @@ function TMAnalyze(){
                 <div class="columns">
                     <div class="column is-full">
                         <div className={Input.instruction}>
-                            <p>Upload an image of your trademark logo (.png, .jpg):</p>
+                            <p>Upload image(s) of your trademark logo and the logo you want to compare to (.png, .jpg):</p>
                         </div>
-                        <div class="file is-boxed">
-                            <label class="file-label">
-                                <input class="file-input" type="file" name="logo"/>
-                                    <span class="file-cta">
-                                        <span class="file-icon">
-                                            <i class="fa fa-upload"></i>
+                        <div className={Input.trademarkLogo}>
+                            <div class="file is-boxed">
+                                <label class="file-label">
+                                    <input class="file-input" type="file" name="logo"/>
+                                        <span class="file-cta">
+                                            <span class="file-icon">
+                                                <i class="fa fa-upload"></i>
+                                            </span>
+                                            <span class="file-label">
+                                                Upload an image of your logo (.png, .jpg)
+                                            </span>
                                         </span>
-                                        <span class="file-label">
-                                            Upload an image of your logo (.png, .jpg)
-                                        </span>
-                                    </span>
-                            </label>
+                                </label>
+                            </div>
                         </div>
-                        <div className={Input.button}>
+                        <div className={Input.trademarkCompare}>
+                            <div class="file is-boxed">
+                                <label class="file-label">
+                                    <input class="file-input" type="file" name="logo"/>
+                                        <span class="file-cta">
+                                            <span class="file-icon">
+                                                <i class="fa fa-upload"></i>
+                                            </span>
+                                            <span class="file-label">
+                                                Upload an image of your logo (.png, .jpg)
+                                            </span>
+                                        </span>
+                                </label>
+                            </div>
+                        </div>
+                        <div className={Input.uploadButton}>
                             <button class="button is-link">Go!</button>
                         </div>
                     </div>
@@ -43,65 +60,6 @@ function TMAnalyze(){
 
             
         </div>
-
-        // <div className={Input.background}>
-        //     <div className={Input.form}>
-        //         <h1 className={Input.title}>Trademark Analyzer Input</h1>
-        //         <h1 className={Input.title}>Trademark Analyzer Input</h1>
-        //         <br></br>
-        //         <form>
-        //             <p>Upload trademark as image:</p>
-        //             <br></br>
-        //             {/* <Uploady destination={{url: process.env.UPLOAD_URL}}>
-        //                 <UploadButton>Upload image</UploadButton>
-        //             </Uploady> */}
-        //             <br></br>
-        //             <br></br>
-        //             <p>Upload word mark:</p>
-                    
-        //             <input type="text" id="word-mark" name="word-mark"></input>
-        //             <br></br>
-        //             <input type="submit" value="Submit"/>
-        //             <br></br>
-        //             <br></br>
-        //             <h1><b>Confusion Index:</b></h1>
-        //             <br></br>
-        //             <p>2. Degree of Attention</p>
-        //             <select>
-        //                 <option value="Low">Low</option>
-        //                 <option value="Medium">Medium</option>
-        //                 <option value="High">High</option>
-        //             </select>
-        //             <br></br>
-        //             <br></br>
-        //             <p>3. How similar are the visual appearances of the signs?</p>
-        //             <select>
-        //                 <option value="None">No Similarity</option>
-        //                 <option value="Low">Low Similarity</option>
-        //                 <option value="High">High Similarity</option>
-        //                 <option value="Identical">Identical</option>
-        //             </select>
-        //             <br></br>
-        //             <br></br>
-        //             <p>4. How similar are the signs phonetically?</p>
-        //             <select>
-        //                 <option value="None">No Similarity</option>
-        //                 <option value="Low">Low Similarity</option>
-        //                 <option value="High">High Similarity</option>
-        //                 <option value="Identical">Identical</option>
-        //             </select>
-        //             <br></br>
-        //             <br></br>
-        //             <p>5. How similar are the signs conceptually?</p>
-        //             <select>
-        //                 <option value="None">No Similarity</option>
-        //                 <option value="Low">Low Similarity</option>
-        //                 <option value="High">High Similarity</option>
-        //                 <option value="Identical">Identical</option>
-        //             </select>
-        //         </form>
-        //     </div>
-        // </div>
     )
 }
 
